beb221a0afe4101d1f049eb2daf4891db7d66157
Jules Fleuren <95241984+JulesFleuren@users.noreply.github.com>
2026-05-23 12:18:38 +0200

    upgrade grib-rs to 0.15

--- rust-wasm/src/overlays.rs
+++ rust-wasm/src/overlays.rs
@@ -1,5 +1,5 @@
 use colorgrad::Gradient;
-use grib::GridDefinitionTemplateValues;
+use grib::{GridDefinitionTemplateValues, GridPointIndex};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::f32;
